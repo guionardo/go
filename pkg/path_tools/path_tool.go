3b51dc7c6504cfce8166ff2b3572d46f8e579dde
Guionardo Furlan <guionardo@gmail.com>
2025-07-09 14:38:46 -0300

    Update path_tool.go

--- pkg/path_tools/path_tool.go
+++ pkg/path_tools/path_tool.go
@@ -4,12 +4,13 @@ import (
 	"os"
 )
 
+// DirExists simply returns true if the pathName is a existing directory
 func DirExists(pathName string) bool {
 	stat, err := os.Stat(pathName)
 	return err == nil && stat.IsDir()
 }
 
-// Create full path, with permissions updated from parent folder.
+// CreatePath Create full path, with permissions updated from parent folder.
 func CreatePath(path string) error {
 	if DirExists(path) {
 		return nil
